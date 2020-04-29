f0223bbdf9a4d38faaa2a478cd5d26ed3cd9fd77
Samet Poyraz <poyraz50_38@hotmail.com>
2020-04-29 23:20:23 +0300

    veri tabanına şifreli veri ekleme ve şifreli verileri çekme programı yüklemesi

---
+++ veritabani_sifreli_veri_yukleme_cekme/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace veritabani_sifreli_veri_yukleme_cekme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_ad = new System.Windows.Forms.TextBox();
+            this.txt_soyad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_mail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_ekle = new System.Windows.Forms.Button();
+            this.btn_listele = new System.Windows.Forms.Button();
+            this.btn_guncelle = new System.Windows.Forms.Button();
+            this.btn_sil = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 183);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(438, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(40, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kisi Ad :";
+            // 
+            // txt_ad
+            // 
+            this.txt_ad.Location = new System.Drawing.Point(100, 26);
+            this.txt_ad.Name = "txt_ad";
+            this.txt_ad.Size = new System.Drawing.Size(100, 20);
+            this.txt_ad.TabIndex = 2;
+            // 
+            // txt_soyad
+            // 
+            this.txt_soyad.Location = new System.Drawing.Point(100, 52);
+            this.txt_soyad.Name = "txt_soyad";
+            this.txt_soyad.Size = new System.Drawing.Size(100, 20);
+            this.txt_soyad.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(20, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Kisi Soyad :";
+            // 
+            // txt_mail
+            // 
+            this.txt_mail.Location = new System.Drawing.Point(100, 78);
+            this.txt_mail.Name = "txt_mail";
+            this.txt_mail.Size = new System.Drawing.Size(100, 20);
+            this.txt_mail.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(32, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Kisi Mail :";
+            // 
+            // btn_ekle
+            // 
+            this.btn_ekle.Location = new System.Drawing.Point(229, 24);
+            this.btn_ekle.Name = "btn_ekle";
+            this.btn_ekle.Size = new System.Drawing.Size(75, 23);
+            this.btn_ekle.TabIndex = 7;
+            this.btn_ekle.Text = "EKLE";
+            this.btn_ekle.UseVisualStyleBackColor = true;
+            this.btn_ekle.Click += new System.EventHandler(this.btn_ekle_Click_1);
+            // 
+            // btn_listele
+            // 
+            this.btn_listele.Location = new System.Drawing.Point(386, 109);
+            this.btn_listele.Name = "btn_listele";
+            this.btn_listele.Size = new System.Drawing.Size(75, 23);
+            this.btn_listele.TabIndex = 8;
+            this.btn_listele.Text = "LİSTELE";
+            this.btn_listele.UseVisualStyleBackColor = true;
+            this.btn_listele.Click += new System.EventHandler(this.btn_listele_Click);
+            // 
+            // btn_guncelle
+            // 
+            this.btn_guncelle.Location = new System.Drawing.Point(229, 52);
+            this.btn_guncelle.Name = "btn_guncelle";
+            this.btn_guncelle.Size = new System.Drawing.Size(75, 23);
+            this.btn_guncelle.TabIndex = 9;
+            this.btn_guncelle.Text = "GÜNCELLE";
+            this.btn_guncelle.UseVisualStyleBackColor = true;
+            this.btn_guncelle.Click += new System.EventHandler(this.btn_guncelle_Click);
+            // 
+            // btn_sil
+            // 
+            this.btn_sil.Location = new System.Drawing.Point(229, 81);
+            this.btn_sil.Name = "btn_sil";
+            this.btn_sil.Size = new System.Drawing.Size(75, 23);
+            this.btn_sil.TabIndex = 10;
+            this.btn_sil.Text = "SİL";
+            this.btn_sil.UseVisualStyleBackColor = true;
+            this.btn_sil.Click += new System.EventHandler(this.btn_sil_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 388);
+            this.Controls.Add(this.btn_sil);
+            this.Controls.Add(this.btn_guncelle);
+            this.Controls.Add(this.btn_listele);
+            this.Controls.Add(this.btn_ekle);
+            this.Controls.Add(this.txt_mail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_soyad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_ad);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_ad;
+        private System.Windows.Forms.TextBox txt_soyad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_mail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_ekle;
+        private System.Windows.Forms.Button btn_listele;
+        private System.Windows.Forms.Button btn_guncelle;
+        private System.Windows.Forms.Button btn_sil;
+    }
+}
+
---
+++ veritabani_sifreli_veri_yukleme_cekme/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace veritabani_sifreli_veri_yukleme_cekme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        MySqlConnection baglanti = new MySqlConnection("Server=localhost;Database=sifreli_veriler;Uid=root;Pwd=root;");
+        void listele()
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.ColumnCount = 4;
+            dataGridView1.Columns[0].Name = "ID";
+            dataGridView1.Columns[1].Name = "ADI";
+            dataGridView1.Columns[2].Name = "SOYADI";
+            dataGridView1.Columns[3].Name = "MAİL";
+            baglanti.Open();
+            MySqlCommand cmd = new MySqlCommand("select kisi_id,kisi_ad,kisi_soyad,kisi_mail from kisiler", baglanti);
+            MySqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                byte[] adcozum = Convert.FromBase64String(dr[1].ToString());
+                string ad = ASCIIEncoding.ASCII.GetString(adcozum);
+                byte[] soyadcozum = Convert.FromBase64String(dr[2].ToString());
+                string soyad = ASCIIEncoding.ASCII.GetString(soyadcozum);
+                byte[] mailcozum = Convert.FromBase64String(dr[3].ToString());
+                string mail = ASCIIEncoding.ASCII.GetString(mailcozum);
+                string[] veriler = { dr[0].ToString(), ad, soyad, mail };
+                dataGridView1.Rows.Add(veriler);
+                Array.Clear(veriler, 0, 3);
+            }
+            baglanti.Close();
+        }
+        private void btn_ekle_Click_1(object sender, EventArgs e)
+        {
+            byte[] addizi = ASCIIEncoding.ASCII.GetBytes(txt_ad.Text);
+            string adsifre = Convert.ToBase64String(addizi);
+
+            byte[] soyaddizi = ASCIIEncoding.ASCII.GetBytes(txt_soyad.Text);
+            string soyadsifre = Convert.ToBase64String(soyaddizi);
+
+            byte[] maildizi = ASCIIEncoding.ASCII.GetBytes(txt_mail.Text);
+            string mailsifre = Convert.ToBase64String(maildizi);
+
+            baglanti.Open();
+            MySqlCommand cmd = new MySqlCommand("insert into kisiler (kisi_ad,kisi_soyad,kisi_mail) values (@ad,@soyad,@mail)", baglanti);
+            cmd.Parameters.AddWithValue("@ad", adsifre);
+            cmd.Parameters.AddWithValue("@soyad", soyadsifre);
+            cmd.Parameters.AddWithValue("@mail", mailsifre);
+            cmd.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Veri Eklendi","Bilgi",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void btn_listele_Click(object sender, EventArgs e)
+        {
+            listele();
+        }
+
+        private void btn_guncelle_Click(object sender, EventArgs e)
+        {
+            byte[] sifreliad = ASCIIEncoding.ASCII.GetBytes(txt_ad.Text);
+            string ad = Convert.ToBase64String(sifreliad);
+            byte[] sifrelisoyad = ASCIIEncoding.ASCII.GetBytes(txt_soyad.Text);
+            string soyad = Convert.ToBase64String(sifrelisoyad);
+            byte[] sifrelimail = ASCIIEncoding.ASCII.GetBytes(txt_mail.Text);
+            string mail = Convert.ToBase64String(sifrelimail);
+            baglanti.Open();
+            MySqlCommand cmd2 = new MySqlCommand("update kisiler set kisi_ad=@ad,kisi_soyad=@soyad,kisi_mail=@mail where kisi_id=@id", baglanti);
+            cmd2.Parameters.AddWithValue("@ad", ad);
+            cmd2.Parameters.AddWithValue("@soyad", soyad);
+            cmd2.Parameters.AddWithValue("@mail", mail);
+            cmd2.Parameters.AddWithValue("@id", id);
+            cmd2.ExecuteNonQuery();
+            MessageBox.Show("Veriler Güncellendi","Güncelleme",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            baglanti.Close();
+            listele();
+        }
+        string id;
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            id = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            txt_ad.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txt_soyad.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            txt_mail.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+        }
+
+        private void btn_sil_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            MySqlCommand cmd3 = new MySqlCommand("delete from kisiler where kisi_id=" + id, baglanti);
+            cmd3.ExecuteNonQuery();
+            MessageBox.Show("Veriler Silindi","Silme",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            baglanti.Close();
+            listele();
+        }
+    }
+}
